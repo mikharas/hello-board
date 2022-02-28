c433b0c9d5b7ec7e35a2e6a1ae5a7b15f01e43c7
Sarah <mikharas0@gmail.com>
2022-02-28 12:07:47 -0500

    Removed redundant code

--- index.js
+++ index.js
@@ -17,19 +17,11 @@ app.use(cors());
 
 if (env === 'production') {
 	app.use(express.static(__dirname + '/client/build'));
-
-	// All routes other than above will go to index.html
-	app.get('*', (req, res) => {
-		res.sendFile(__dirname + '/client/build/index.html');
-	});
 }
 
 app.use('/api/users', userRoutes);
 app.use('/api/boards', boardRoutes);
 
-app.use((req, res, next) => {
-	res.sendFile(path.resolve('public', 'index.html'));
-});
 
 app.use((req, res, next) => {
 	const error = new HttpError('Route doens\'t exist', 404);
